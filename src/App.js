7412f56e499d2b294a7d5853594d3a7ee07446cb
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-24 01:33:40 +0530

    routes and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Project from "./pages/project/Project";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} exact />
+            <Route path='/create' element={<Create />} />
+            <Route path='/projects/:id' element={<Project />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
