f82c5c26eb7435313152ce77738236a771d2e28f
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-24 17:53:20 +0530

    SignUp Component

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
 import "./Signup.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className='auth-form'>
+      <h2>Sign Up</h2>
+      <label>
+        <span>Email </span>
+        <input
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <span>Password </span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </label>
+      <label>
+        <span>Display Name </span>
+        <input
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+          required
+        />
+      </label>
+      <label>
+        <span>Display Picture </span>
+        <input type='file' required />
+      </label>
+      <button className='btn'>SignUp</button>
+    </form>
+  );
 };
 
 export default Signup;
