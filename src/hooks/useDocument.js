490c1edcc138e27048c2ade77ff9bbffb1728f04
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-26 20:04:09 +0530

    Project details Page

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { database } from "../firebase/config";
 
-export const useDocument = ({ collection, id }) => {
+export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null);
   const [error, setError] = useState(null);
 
@@ -10,8 +10,12 @@ export const useDocument = ({ collection, id }) => {
 
     const unsub = ref.onSnapshot(
       (snapshot) => {
-        setDocument({ ...snapshot.data(), id: snapshot.id });
-        setError(null);
+        if (snapshot.data()) {
+          setDocument({ ...snapshot.data(), id: snapshot.id });
+          setError(null);
+        } else {
+          setError("No Such Document Exist");
+        }
       },
       (err) => {
         console.log(err.message);
