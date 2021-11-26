e14713b9f52593db2717e06f2a7d451bda0fbffa
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-26 23:22:37 +0530

    Project Summary Component

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import { useDocument } from "../../hooks/useDocument";
+import ProjectSummary from "./ProjectSummary";
 
 import "./Project.css";
 
@@ -17,7 +18,7 @@ const Project = () => {
 
   return (
     <div className='project-details'>
-      <h1>{document.name}</h1>
+      <ProjectSummary project={document} />
     </div>
   );
 };
